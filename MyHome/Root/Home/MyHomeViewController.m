47c24bb0c2b7a0e58ce0eccc9a8d94c3463e82a3
977616908 <cdp@CDPdeMac-mini.lan>
2015-08-05 11:39:56 +0800

    ctd

--- MyHome/Root/Home/MyHomeViewController.m
+++ MyHome/Root/Home/MyHomeViewController.m
@@ -9,6 +9,7 @@
 #import "MyHomeViewController.h"
 
 @interface MyHomeViewController ()
+- (IBAction)onHomeClick:(id)sender;
 
 @end
 
@@ -16,7 +17,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBarHidden=YES;
+}
+
+
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    self.navigationController.navigationBarHidden=NO;
+}
+
+- (IBAction)onHomeClick:(id)sender {
+    PSLog(@"---[%d]---",[sender tag]);
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +43,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
